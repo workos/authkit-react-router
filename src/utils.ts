f5174e490a565e8dddbd380740d2dd69bab9d013
Nick Nisi <nick.nisi@workos.com>
2025-03-26 19:13:30 -0500

    fix: pass through non-json responses without modifying the body

--- src/utils.ts
+++ src/utils.ts
@@ -38,6 +38,16 @@ export function isResponse(response: unknown): response is Response {
 }
 
 /**
+ * Returns true if the response is a JSON response.
+ * This is determined by checking if the Content-Type header includes 'application/json'.
+ * @param res - The response to check.
+ * @returns True if the response is a JSON response.
+ */
+export function isJsonResponse(res: Response): boolean {
+  return !!res.headers.get('Content-Type')?.includes('application/json');
+}
+
+/**
  * Checks if the data is a DataWithResponseInit object.
  * @param data - The data to check.
  * @returns True if the data is a DataWithResponseInit object.
