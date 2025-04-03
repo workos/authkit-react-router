c0de4763b704fc69cde4b104413d7422a59a4c67
Nick Nisi <nick.nisi@workos.com>
2025-04-03 10:02:40 -0500

    put exports inline

--- src/auth.ts
+++ src/auth.ts
@@ -1,16 +1,14 @@
 import { getAuthorizationUrl } from './get-authorization-url.js';
 import { terminateSession } from './session.js';
 
-async function getSignInUrl(returnPathname?: string) {
+export async function getSignInUrl(returnPathname?: string) {
   return getAuthorizationUrl({ returnPathname, screenHint: 'sign-in' });
 }
 
-async function getSignUpUrl(returnPathname?: string) {
+export async function getSignUpUrl(returnPathname?: string) {
   return getAuthorizationUrl({ returnPathname, screenHint: 'sign-up' });
 }
 
-async function signOut(request: Request) {
+export async function signOut(request: Request) {
   return await terminateSession(request);
 }
-
-export { getSignInUrl, getSignUpUrl, signOut };
