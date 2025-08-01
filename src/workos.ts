157f7bf72ee379b0587b9e50ba9f364312107712
Nick Nisi <nick.nisi@workos.com>
2025-08-01 10:27:20 -0500

    v0.7.0

--- src/workos.ts
+++ src/workos.ts
@@ -2,7 +2,7 @@ import { WorkOS } from '@workos-inc/node';
 import { getConfig } from './config.js';
 import { lazy } from './utils.js';
 
-const VERSION = '0.6.1';
+const VERSION = '0.7.0';
 
 /**
  * Create a WorkOS instance with the provided API key and optional settings.
