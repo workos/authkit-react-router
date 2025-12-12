948067f80c8381d9c221e75f1e50a3f1c858b42c
Chance Strickland <hi@chance.dev>
2025-12-12 07:55:53 -0800

    export

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { getSignInUrl, getSignUpUrl, signOut, switchToOrganization, withAuth } from './auth.js';
 import { authLoader } from './authkit-callback-route.js';
 import { configure, getConfig } from './config.js';
-import { authkitLoader, refreshSession } from './session.js';
+import { authkitLoader, refreshSession, saveSession } from './session.js';
 import { getWorkOS } from './workos.js';
 
 export {
@@ -14,6 +14,7 @@ export {
   getSignUpUrl,
   getWorkOS,
   refreshSession,
+  saveSession,
   signOut,
   switchToOrganization,
 };
